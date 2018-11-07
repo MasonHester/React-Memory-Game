8104435ed77f49b559fd42e7997cc972636d785e
Mason Hester <masonhester@Masons-MacBook-Pro.local>
2018-11-06 20:53:16 -0500

    Shuffle for images working

--- src/App.js
+++ src/App.js
@@ -12,29 +12,56 @@ class App extends Component {
     bestScore: 0
   }
 
+  shuffleImages = () => {
+    let shuffledArray = this.state.signs;
+    let counter = this.state.signs.length;
+    
+
+    while (counter > 0) {
+        console.log(`==============${counter}==============`);
+        const randomIndex = Math.floor(Math.random() * counter);
+
+        counter = counter - 1;
+
+        let dataForSwap = shuffledArray[counter];
+        console.log(`Data for swap: ${dataForSwap}`);
+        console.log(`Before Swap: ${shuffledArray}`);
+        shuffledArray[counter] = shuffledArray[randomIndex];
+        console.log(`Mid Swap: ${shuffledArray}`);
+        shuffledArray[randomIndex] = dataForSwap;
+        console.log(`After Swap: ${shuffledArray}`);
+    };
+
+    return shuffledArray
+  };
+
+  // const shuffleData = (shuffledArray) => {
+//     
+// };
+
+
   handleScoreChange = (userGuess, id) => {
     if (userGuess === true) {
       const score = this.state.currentScore + 1
       this.setState({
-        signs: images,
+        signs: this.shuffleImages(),
         clickedImages: [...this.state.clickedImages, id],
         currentScore: score,
         bestScore: score > this.state.bestScore ? score : this.state.bestScore 
       })
     } else {
       this.setState({
-        signs: images,
+        signs: this.shuffleImages(),
         clickedImages: [],
         currentScore: 0
       })
     }
+
+    
+
     if (this.state.clickedImages.length >= this.state.signs.length) {
       console.log("win")
     }
-  }
-
-  checkIfClicked = (id) => {
-    return this.state.clickedImages.includes(id);
   }
 
   handleClick = (id) => {
