2c46d04cbcca16aee76bccf123d4194ebf1556c1
Mason Hester <masonhester@Masons-MacBook-Pro.local>
2018-11-24 11:45:17 -0500

    Updates

---
+++ precache-manifest.3c624f92115161787ce13c86fa1996ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "3b7de53f8832b094cc88",
+    "url": "/react-memory-game/static/js/main.3b7de53f.chunk.js"
+  },
+  {
+    "revision": "7ccad06e8a96e8ae6253",
+    "url": "/react-memory-game/static/js/1.7ccad06e.chunk.js"
+  },
+  {
+    "revision": "3b7de53f8832b094cc88",
+    "url": "/react-memory-game/static/css/main.bf6b9031.chunk.css"
+  },
+  {
+    "revision": "c2b2d19cdaea33d6dc978a08277b04be",
+    "url": "/react-memory-game/index.html"
+  }
+];
